ae96398a21b20fb6dea23cfe02203a4f8b612645
Андрей <anastasiiapegar@gmail.com>
2021-07-12 10:18:04 +0300

    cors bug fixed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 // const port = 3011;
 app.use(cors());
 app.use(cors({ origin: "http:\\localhost:3000" }))
+app.use(cors({ origin: "https:\\MoskvinAndrew.github.io" }))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
